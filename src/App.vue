2cab6b28ee9e6f4c20ba80a6957a3f10a3d3b0b6
jinzhuming <jinzhuming0308@gmail.com>
2018-04-04 18:36:41 +0800

    基础性配置 sass；

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,11 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+// 基础颜色与 mixin 都定义在 index，index 必须在所有scss之前引用
+@import url('../static/index.scss');
+@import url('../static/normalize.scss');
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
