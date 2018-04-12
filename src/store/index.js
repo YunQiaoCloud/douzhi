dfc3dcf0a4bc49ce757758c67364b3664d8f8b31
jinzhuming <jinzhuming0308@gmail.com>
2018-04-12 18:09:47 +0800

    更改错误的数据获取方式

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import moviesStore from './modules/moviesStore'
+import movies from './movies'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    moviesStore
+    movies
   }
 })
--- src/store/movies.js
+++ src/store/movies.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 //state
 const state = {
-  movies: [{
+  list: [{
     title: '',
     mainland_pubdate: '',
     images: {}
@@ -11,8 +11,8 @@ const state = {
 
 //mutations
 const mutations = {
-  setMovies(state, movies) {
-    state.movies = movies
+  setMovies(state, movieList) {
+    state.list = movieList
   }
 }
 
@@ -20,8 +20,8 @@ const mutations = {
 const actions = {
   async getMovies({ commit }) {
     const res = await axios.get('/api/movie/in_theaters?apikey=0b2bdeda43b5688921839c8ecb20399b&city=%E5%8C%97%E4%BA%AC&start=0&count=10&client=&udid=')
-    const movies = res.data.subjects
-    commit('setMovies', movies)
+    const movieList = res.data.subjects
+    commit('setMovies', movieList)
   }
 }
 
